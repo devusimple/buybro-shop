7ccbd65fa5e5174303dfb599db933ab245a867e5
devusimple <nahidhasan195446@gmail.com>
2026-03-03 03:49:59 +0600

    v2.0.8

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,21 +1,28 @@
 import { AdminLayout } from '@/components/admin/AdminLayout';
 import Auth from '@/components/auth/auth';
 import db from '@/lib/db';
+import { Loader2 } from 'lucide-react';
 
 interface AdminRootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AdminRootLayout({ children }: AdminRootLayoutProps) {
-  return (
-    <>
-      <db.SignedIn>
-        <AdminLayout>{children}</AdminLayout>
-      </db.SignedIn>
-      <db.SignedOut>
-        <Auth />
-      </db.SignedOut>
-    </>
 
-  )
+  const { isLoading, user, error } = db.useAuth();
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <Loader2 className='animate-spin' />
+      </div>
+    ) // or a loading spinner
+  }
+  if (error) {
+    return <div className="p-4 text-red-500">Uh oh! {error.message}</div>;
+  }
+  if (user) {
+    return <AdminLayout>{children}</AdminLayout>
+      ;
+  }
+  return <Auth />;
 }
